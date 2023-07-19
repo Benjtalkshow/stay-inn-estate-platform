55d73dcb7c07c11c7af5f2f743ca2929b7401270
Benjtalkshow <chinedubenj@gmail.com>
2023-07-19 00:40:23 -0700

    added javascript Observer Intersection API for lazy loading/scroll on view effect

--- home/index.js
+++ home/index.js
@@ -3,6 +3,7 @@ const xmark = document.querySelector(".xmark");
 const navBar = document.querySelector(".navbar");
 const linkContainer = document.querySelector(".link_container");
 
+// javascript code for hamburger menu
 bar.addEventListener("click", (e) => {
   e.preventDefault();
   navBar.classList.add("display");
@@ -23,7 +24,49 @@ xmark.addEventListener("click", (e) => {
 });
 
 
+// Function to handle the intersection of the observed elements
+class BootStrapAnimation {
+  #section;
+  constructor(section) {
+    this.#section = section;
+    this.#observeIntersection();
+  }
 
+  #handleIntersection(entries, observer) {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    }
+  }
+
+  #observeIntersection() {
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver(
+      this.#handleIntersection,
+      observerOptions
+    );
+    observer.observe(this.#section);
+  }
+}
+
+function animateOnIt() {
+  const sections = document.querySelectorAll(".lazy-section");
+  for (const section of sections) {
+    new BootStrapAnimation(section);
+  }
+}
+
+animateOnIt();
+
+
+// Navbar responsiveness with the help of javascript method (matchMedia())
 const mediaQueryActive = window.matchMedia('(min-width: 962px)');
 const displayHamburger = window.matchMedia('(max-width:961px)');
 const checkMediaQueryActive = (mq) => {
